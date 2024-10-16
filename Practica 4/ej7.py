0e21bb925d49fcf6a63f41644a03292652319bd9
administrador <administrador@Xubuntu22.salesianas.org>
2024-10-16 14:17:48 +0200

    16/10/24

--- Practica 4/ej7.py
+++ Practica 4/ej7.py
@@ -8,17 +8,17 @@ while True:
 
 maximo=-1000
 
-for i in range(len(nombres)):
-    buscar=nombres[i]
+for i in nombres:
+    buscar=i
     
-    if nombres[i] in nombresFound:
+    if i in nombresFound:
         continue
     
-    nombresFound.append(nombres[i])
+    nombresFound.append(i)
     x=0
 
-    for j in range(len(nombres)):
-        if buscar==nombres[j]:
+    for j in nombres:
+        if buscar==j:
             x+=1
     print(buscar+": "+str(x))
     
